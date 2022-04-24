b8e2402674dfc3063e5a76aa97212bce4137bb3d
KING-Samm <kcsamm11@gmail.com>
2022-04-24 10:32:55 +0100

    Created deploy scripts

---
+++ scripts/deploy.js
@@ -0,0 +1,52 @@
+const { ethers } = require("hardhat");
+
+// async function main()
+// {
+//     const [ deployer ] = await ethers.getSigners();
+//     console.log(`Deploying contract with address: ${ deployer.address }`);
+
+//     const balance = deployer.getBalance();
+//     console.log(`Account balance: ${ (await balance).toString() } `);
+
+//     const Token = await ethers.getContractFactory("Token");
+//     const token = await Token.deploy();
+//     console.log(`Token address: ${ token.address }`);
+// }
+
+// main()
+//     .then(() => process.exit(0))
+//     .catch(error => 
+//     {
+//         console.error(error);
+//         process.exit(1);
+//     });
+
+
+const main = async () =>
+{
+    const [ deployer ] = await ethers.getSigners();
+    console.log(`Deploying contract with address: ${ deployer.address }`);
+
+    const balance = deployer.getBalance();
+    console.log(`Account balance: ${ (await balance).toString() } `);
+
+    const Token = await ethers.getContractFactory("Token");
+    const token = await Token.deploy();
+    console.log(`Token address: ${ token.address }`);
+}
+
+const callMain = async () => 
+{
+    try 
+    {
+        await main();
+        process.exit(0);
+    } 
+    catch (error) 
+    {
+        console.error(error);
+        process.exit(1);
+    }
+}
+
+callMain();
